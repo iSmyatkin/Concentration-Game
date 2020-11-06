aaaebd848965825a4331d1dca0fb7567dcc12d14
Ivan Smyatkin <ismyatkin@gmail.com>
2020-11-06 13:45:41 +0300

    1. basic design (stack views) 2. ConcentrationViewController + outlets

---
+++ Concentration Game/ConcentrationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ConcentrationViewController.swift
+//  Concentration Game
+//
+//  Created by Иван Смяткин on 06.11.2020.
+//
+
+import UIKit
+
+class ConcentrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+}
+
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func selectCard(_ sender: UIButton) {
+    }
+    
+}
